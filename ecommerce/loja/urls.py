bf91ef38ae7af78d71654bab8ba68e17bc393e33
Sivaldi vieira <sivaldovieiradealmeida@gmail.com>
2025-03-26 19:15:01 -0300

    6. Criando várias páginas para o site

--- ecommerce/loja/urls.py
+++ ecommerce/loja/urls.py
@@ -4,5 +4,13 @@ from.views import *
 
 urlpatterns = [
     path('', homepage, name='homepage'),
+    path('loja/', loja, name='homepage'),
+    path('minhaconta/', minha_conta, name='minha_conta'),
+    path('login', login, name='login'),
+    path('carrinho', carrinho, name='carrinho'),
+    path('checkout', checkout, name='checkout'),
 
 ]
+
+
+#depois disso criar funcoes na views

840f5c7d416cc429e3350c4ee375a2479af8c4ef
Sivaldi vieira <sivaldovieiradealmeida@gmail.com>
2025-03-27 10:05:31 -0300

    10. Barra de navegação e links

--- ecommerce/loja/urls.py
+++ ecommerce/loja/urls.py
@@ -4,10 +4,9 @@ from .views import *
 urlpatterns = [
     path('', homepage, name='homepage'),
 
-    
     # path('homepage/', homepage, name='homepage'),
     path('loja/', loja, name='loja'),
-    path('minhaconta/', minha_conta, name='minhaconta'),
+    path('minhaconta/', minha_conta, name='minha_conta'),
     path('login/', login, name='login'),
     path('carrinho/', carrinho, name='carrinho'),
     path('checkout/', checkout, name='checkout'),

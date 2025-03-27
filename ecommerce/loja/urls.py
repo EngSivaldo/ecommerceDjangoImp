de1f32891557e1b2591e3722234732b7e71cd067
Sivaldi vieira <sivaldovieiradealmeida@gmail.com>
2025-03-26 21:12:50 -0300

    . Uma base html - editando páginas de forma dinâmica

--- ecommerce/loja/urls.py
+++ ecommerce/loja/urls.py
@@ -1,16 +1,13 @@
 from django.urls import path
-#importar tudo da views( ex: homepage)
-from.views import *
+from .views import *
 
 urlpatterns = [
     path('', homepage, name='homepage'),
-    path('loja/', loja, name='homepage'),
-    path('minhaconta/', minha_conta, name='minha_conta'),
-    path('login', login, name='login'),
-    path('carrinho', carrinho, name='carrinho'),
-    path('checkout', checkout, name='checkout'),
-
+    path('homepage/', homepage, name='homepage'),
+    path('loja/', loja, name='loja'),
+    path('minhaconta/', minha_conta, name='minhaconta'),
+    path('login/', login, name='login'),
+    path('carrinho/', carrinho, name='carrinho'),
+    path('checkout/', checkout, name='checkout'),
 ]
-
-
 #depois disso criar funcoes na views

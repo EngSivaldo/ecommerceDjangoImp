018a5302e766ebff153311e7d726673a0ea86280
Sivaldi vieira <sivaldovieiradealmeida@gmail.com>
2025-03-26 21:57:57 -0300

    . Arquivos static

--- ecommerce/ecommerce/settings.py
+++ ecommerce/ecommerce/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 
+#--puxar os arquivos do static-->
+import os
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -117,6 +121,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+#--caminho para os arquivos do static-->
+STATICFILES_DIRS = [
+  os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
--- ecommerce/loja/urls.py
+++ ecommerce/loja/urls.py
@@ -3,7 +3,9 @@ from .views import *
 
 urlpatterns = [
     path('', homepage, name='homepage'),
-    path('homepage/', homepage, name='homepage'),
+
+    
+    # path('homepage/', homepage, name='homepage'),
     path('loja/', loja, name='loja'),
     path('minhaconta/', minha_conta, name='minhaconta'),
     path('login/', login, name='login'),
